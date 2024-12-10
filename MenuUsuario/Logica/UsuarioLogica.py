c5585a81c995acb5daec3223b05b4083bb977696
Kraneowatt <gasparlopezjeifetz@gmail.com>
2024-12-10 19:35:48 -0300

    Update UsuarioLogica.py

--- MenuUsuario/Logica/UsuarioLogica.py
+++ MenuUsuario/Logica/UsuarioLogica.py
@@ -2,6 +2,7 @@ from tkinter import messagebox
 import tkinter as tk
 from tkinter import messagebox
 from datetime import datetime, date, timedelta
+import numpy as np
 import pandas as pd
 from tkinter import ttk
 from functools import partial
